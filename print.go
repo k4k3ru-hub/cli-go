37bf970209d621b5f4b3bec9de3184b36ac27cab
k4k3ru-hub <info@k4k3ru.com>
2025-03-17 16:06:19 +0900

    Updated Print func

--- print.go
+++ print.go
@@ -17,17 +17,31 @@ import (
 func PrintTable(headers []string, rows [][]interface{}) {
 	const padding = 2
 	dataWriter := tabwriter.NewWriter(os.Stdout, 0, 0, padding, ' ', 0)
-	separatorWriter := tabwriter.NewWriter(os.Stdout, 0, 0, padding, '-', 0)
+
+	// Calculate maximum column widths.
+	colWidths := make([]int, len(headers))
+	for i, header := range headers {
+		colWidths[i] = len(header)+padding
+	}
+	for _, row := range rows {
+		for i, col := range row {
+			colStr := fmt.Sprintf("%v", col)
+			if len(colStr)+padding > colWidths[i] {
+				colWidths[i] = len(colStr)+padding
+			}
+		}
+	}
+	totalWidth := 0
+	for _, width := range colWidths {
+		totalWidth += width
+	}
 
 	// Headers
 	for _, header := range headers {
 		fmt.Fprintf(dataWriter, "%s\t", header)
-		fmt.Fprintf(separatorWriter, "%s\t", strings.Repeat("-", len(header)))
 	}
 	fmt.Fprintln(dataWriter)
-	fmt.Fprintln(separatorWriter)
-	dataWriter.Flush()
-	separatorWriter.Flush()
+	fmt.Fprintf(dataWriter, "%s\n", strings.Repeat("-", totalWidth))
 
 	// Rows
 	for _, row := range rows {
